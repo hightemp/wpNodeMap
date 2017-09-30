ef7dc197d226b92818b6b57d371fcd3fa75c4777
hightemp <hightemp.unknown@gmail.com>
2017-09-30 16:47:25 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-window.document.addEventListener("DOMContentLoaded", function() {
-    console.log("ready");
+"use strickt";
+
+window.document.addEventListener("DOMContentLoaded", function()
+{
+    window.objBlocks = {};    
+    
+    var TBlock = function(sName, sTitle, objDOMParent, objAttributes)
+    {
+        this.sTitle = sTitle;
+        this.objDOMParent = objDOMParent;
+        
+        this.fnRender = function()
+        {
+            var sBlocks = "<div id='"+sName+"' class='block'><div class='title'></div><div class='content'></div></div>";
+            this.objDOMParent.insertAdjacentHTML('beforeEnd', sBlocks);
+        }
+        
+        this.fnRender();
+    }
+    
+    window.fnAddBlock = function(sName, sTitle, objDOMParent, objAttributes)
+    {
+        if (window.objBlocks[sName] !== undefined)
+            throw "Block "+sName+" already exists";
+        window.objBlocks[sName] = new TBlock(sName, sTitle, objAttributes);
+    }
+    
+    window.fnAddBlock("test", "Test block", document.body);
 });
